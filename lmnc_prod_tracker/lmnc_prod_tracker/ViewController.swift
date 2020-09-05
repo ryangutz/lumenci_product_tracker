a5bdf7cf6c9c6027774c28b31b08c4c65d723b8e
Ryan Gutierrez <ryanbgutz@Ryans-MacBook-Pro.local>
2020-09-05 17:18:22 -0500

    adding project

---
+++ lmnc_prod_tracker/lmnc_prod_tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lmnc_prod_tracker
+//
+//  Created by Ryan Gutierrez on 9/5/20.
+//  Copyright © 2020 Ryan Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
